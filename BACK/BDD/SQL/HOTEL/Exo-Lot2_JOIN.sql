9e4cf5db2e06fd198a13e707c87b3e81c8f80d2c
Rosariot80090 <grosariot@yahoo.fr>
2022-10-11 22:41:38 +0200

    Exo_JOIN_9

--- BACK/BDD/SQL/HOTEL/Exo-Lot2_JOIN.sql
+++ BACK/BDD/SQL/HOTEL/Exo-Lot2_JOIN.sql
@@ -19,21 +19,26 @@ INNER JOIN chambre ON cha_hot_id = hot_id;
 --9 - Afficher la liste des chambres de plus d'une place dans des hôtels situés sur la ville de Bretou
 --Le résultat doit faire apparaître le nom de l’hôtel, la catégorie, la ville, le numéro de la chambre et sa capacité
 
-SELECT hot_nom, hot_categorie, hot_ville="Bretou", cha_numero, cha_capacite
+SELECT hot_nom, hot_categorie, hot_ville, cha_numero, cha_capacite
 FROM hotel
-INNER JOIN chambre ON cha_capacite>1;
+INNER JOIN chambre ON cha_capacite>1 AND hot_ville = "Bretou";
 
 
 --10 - Afficher la liste des réservations avec le nom des clients
 --Le résultat doit faire apparaître le nom du client, le nom de l’hôtel, la date de réservation
 
-
-
+SELECT cli_nom, hot_nom, res_date
+FROM client, reservation, hotel
+where client.cli_id = reservation.res_cli_id;
 --11 - Afficher la liste des chambres avec le nom de l’hôtel et le nom de la station
 --Le résultat doit faire apparaître le nom de la station, le nom de l’hôtel, le numéro de la chambre et sa capacité
 
-
+SELECT sta_nom, hot_nom, cha_numero, cha_capacite
+FROM station
+JOIN hotel ON hotel.hot_sta_id = station.sta_id
+JOIN chambre ON chambre.cha_hot_id = hotel.hot_id;
 
 --12 - Afficher les réservations avec le nom du client et le nom de l’hôtel avec datediff
 --Le résultat doit faire apparaître le nom du client, le nom de l’hôtel, la date de début du séjour et la durée du séjour
 
+SELECT cli_nom, hot_nom, res_date_debut, res_date_fin
