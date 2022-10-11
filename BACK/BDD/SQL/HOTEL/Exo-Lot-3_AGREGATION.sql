7c7aaf5d49736067a66cda3bb23c8e32e08488d1
Gerart rosariot <grosariot@yahoo.fr>
2022-10-11 13:51:15 +0200

    Merge branch 'master' of https://github.com/Rosariot80090/depot_local

--- BACK/BDD/SQL/HOTEL/Exo-Lot-3_AGREGATION.sql
+++ BACK/BDD/SQL/HOTEL/Exo-Lot-3_AGREGATION.sql
@@ -1,14 +1,12 @@
-Lot 3 : fonctions d'agrégation
+--Lot 3 : fonctions d'agrégation
 
-13 - Compter le nombre d’hôtel par station
+--13 - Compter le nombre d’hôtel par station
 
-14 - Compter le nombre de chambres par station
+--14 - Compter le nombre de chambres par station
 
-15 - Compter le nombre de chambres par station ayant une capacité > 1
+--15 - Compter le nombre de chambres par station ayant une capacité > 1
 
-16 - Afficher la liste des hôtels pour lesquels Mr Squire a effectué une réservation
+--16 - Afficher la liste des hôtels pour lesquels Mr Squire a effectué une réservation
 
-17 - Afficher la durée moyenne des réservations par station
-
-
+--17 - Afficher la durée moyenne des réservations par station
 
