a81f6dfb5d77052dd989f574d25214b3d2980f82
Gerart rosariot <grosariot@yahoo.fr>
2022-10-11 16:57:24 +0200

    exo JOIN 9

--- BACK/BDD/SQL/HOTEL/Exo-Lot2_JOIN.sql
+++ BACK/BDD/SQL/HOTEL/Exo-Lot2_JOIN.sql
@@ -3,17 +3,25 @@
 -- 7 - Afficher la liste des hôtels avec leur station
 -- Le résultat doit faire apparaître le nom de la station, le nom de l’hôtel, la catégorie, la ville
 
-SELECT sta_nom
+SELECT sta_nom, hot_nom, hot_categorie, hot_ville
+FROM station
+INNER JOIN hotel  ON hot_sta_id = hot_id;
+
 
 
 --8 - Afficher la liste des chambres et leur hôtel
 --Le résultat doit faire apparaître le nom de l’hôtel, la catégorie, la ville, le numéro de la chambre
 
-
+SELECT hot_nom, hot_categorie, hot_ville, cha_numero
+FROM hotel
+INNER JOIN chambre ON cha_hot_id = hot_id;
 
 --9 - Afficher la liste des chambres de plus d'une place dans des hôtels situés sur la ville de Bretou
 --Le résultat doit faire apparaître le nom de l’hôtel, la catégorie, la ville, le numéro de la chambre et sa capacité
 
+SELECT hot_nom, hot_categorie, hot_ville="Bretou", cha_numero, cha_capacite
+FROM hotel
+INNER JOIN chambre ON cha_capacite>1;
 
 
 --10 - Afficher la liste des réservations avec le nom des clients
