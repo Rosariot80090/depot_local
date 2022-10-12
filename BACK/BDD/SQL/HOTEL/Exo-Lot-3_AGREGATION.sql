d8aad4022bc055a1bd244048f537daa0a7b6e4dc
Gerart rosariot <grosariot@yahoo.fr>
2022-10-12 16:49:59 +0200

    Exo13_lot3

--- BACK/BDD/SQL/HOTEL/Exo-Lot-3_AGREGATION.sql
+++ BACK/BDD/SQL/HOTEL/Exo-Lot-3_AGREGATION.sql
@@ -1,6 +1,9 @@
 --Lot 3 : fonctions d'agrégation
 
 --13 - Compter le nombre d’hôtel par station
+SELECT COUNT(hot_sta_id), sta_nom, hot_nom
+FROM hotel
+JOIN station ON station.sta_id = hotel.hot_sta_id;
 
 --14 - Compter le nombre de chambres par station
 
