d130266bc83c19e588306cbe500da9fe93886562
Gerart rosariot <grosariot@yahoo.fr>
2022-10-17 16:43:35 +0200

    modif Exo17-lot3

--- BACK/BDD/SQL/HOTEL/Exo17-lot3.sql
+++ BACK/BDD/SQL/HOTEL/Exo17-lot3.sql
@@ -1,7 +1,4 @@
 /* Afficher la durée moyenne des réservations par station */
-/* Données nécessaires: 
-1) table des RESERVATIONS, avec res_date_debut et res_date_fin
-2) table des HOTELS
 
 SELECT sta_nom,AVG(DATEDIFF(res_date_fin,res_date_debut))AS Moyenne
 FROM reservation,station
