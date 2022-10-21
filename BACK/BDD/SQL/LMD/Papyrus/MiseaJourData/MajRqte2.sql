08791a20d2db7959a3b332df1a47821603aa4c26
Gerart rosariot <grosariot@yahoo.fr>
2022-10-21 12:01:48 +0200

    Exo2_MaJ

---
+++ BACK/BDD/SQL/LMD/Papyrus/MiseaJourData/MajRqte2.sql
@@ -0,0 +1,16 @@
+/* 1.Appliquer une augmentation de tarif de 4% pour le prix 1, et de 2% pour le prix2, pour le fournisseur 9180.*/
+
+
+/* 2.Dans la table vente, mettre à jour le prix2 des articles dont le prix2 est nul, en affectant la valeur du prix1.*/
+	
+	UPDATE vente
+	SET vente.prix2=vente.prix1
+	WHERE vente.prix2=0
+
+/* 3.Mettre à jour le champ obscom, en renseignant ***** pour toutes les commandes dont le fournisseur a un indice de satisfaction inférieur à 5.*/
+
+/* 4.Supprimer le produit "I110".*/   
+   
+   
+
+ 
